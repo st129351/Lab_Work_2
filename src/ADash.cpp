a84b316fa190c27f4aa31c7bad7a9e29db42a43c
Lichkovaha <st129351@student.spbu.ru>
2025-05-06 13:47:36 +0000

    change amulet for dash

--- src/ADash.cpp
+++ src/ADash.cpp
@@ -1,8 +1,13 @@
+//Lichkovaha Daniil
+//st129351@student.spbu.ru
+//LabWork2
 #include "ADash.h"
 #include "GameField.h"
 #include "Player.h"
 
-ADash::ADash(GameField& f) : Amulet(25, false, "dash", "incredible speed, press 'k'", 8), field(f) {}
+ADash::ADash() : Amulet(25, false, "dash", "incredible speed, press 'k'", 8), field(nullptr) {}
+
+ADash::ADash(GameField& f) : Amulet(25, false, "dash", "incredible speed, press 'k'", 8), field(&f) {}
 
 ADash::~ADash() {};
 
@@ -10,9 +15,9 @@ void ADash::UpdateDashVisual()
 {
     for (auto i = dash_visual.begin(); i != dash_visual.end();)
     {
-        if (field.getSymbol(i->x, i->y) == '|' || field.getSymbol(i->x, i->y) == '-')
+        if (field->getSymbol(i->x, i->y) == '|' || field->getSymbol(i->x, i->y) == '-')
         {
-            field.setSymbol(i->x, i->y, ' ');
+            field->setSymbol(i->x, i->y, ' ');
         }
         i = dash_visual.erase(i);
     }
@@ -20,7 +25,8 @@ void ADash::UpdateDashVisual()
 
 void ADash::use(Player& player)
 {
-    char symb = field.getInputSymb();
+
+    char symb = field->getInputSymb();
     int new_x = player.getX_pos();
     int new_y = player.getY_pos();
 
@@ -29,15 +35,15 @@ void ADash::use(Player& player)
     {
         new_y--;
         count += 1;
-        while (count <= 2 && field.getSymbol(new_x, new_y) != '#' && field.getSymbol(new_x, new_y) != 's' && field.getSymbol(new_x, new_y) != 'b' && field.getSymbol(new_x, new_y) != 'Y' && field.getSymbol(new_x, new_y) != 'E' && field.getSymbol(new_x, new_y) != 'T')
+        while (count <= 2 && field->getSymbol(new_x, new_y) != '#' && field->getSymbol(new_x, new_y) != 's' && field->getSymbol(new_x, new_y) != 'b' && field->getSymbol(new_x, new_y) != 'Y' && field->getSymbol(new_x, new_y) != 'E' && field->getSymbol(new_x, new_y) != 'T')
         {
             if (count == 1 || count == 2)
             {
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '|');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '|');
                 dash_visual.push_back({player.getX_pos(), player.getY_pos()});
                 player.setX_pos(new_x);
                 player.setY_pos(new_y);
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '@');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '@');
             }
             new_y--;
             count += 1;
@@ -47,15 +53,15 @@ void ADash::use(Player& player)
     {
         new_x--;
         count += 1;
-        while (count <= 2 && field.getSymbol(new_x, new_y) != '#' && field.getSymbol(new_x, new_y) != 's' && field.getSymbol(new_x, new_y) != 'b' && field.getSymbol(new_x, new_y) != 'Y' && field.getSymbol(new_x, new_y) != 'E' && field.getSymbol(new_x, new_y) != 'T')
+        while (count <= 2 && field->getSymbol(new_x, new_y) != '#' && field->getSymbol(new_x, new_y) != 's' && field->getSymbol(new_x, new_y) != 'b' && field->getSymbol(new_x, new_y) != 'Y' && field->getSymbol(new_x, new_y) != 'E' && field->getSymbol(new_x, new_y) != 'T')
         {
             if (count == 1 || count == 2)
             {
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '-');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '-');
                 dash_visual.push_back({player.getX_pos(), player.getY_pos()});
                 player.setX_pos(new_x);
                 player.setY_pos(new_y);
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '@');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '@');
             }
             new_x--;
             count += 1;
@@ -65,15 +71,15 @@ void ADash::use(Player& player)
     {
         new_y++;
         count += 1;
-        while (count <= 2 && field.getSymbol(new_x, new_y) != '#' && field.getSymbol(new_x, new_y) != 's' && field.getSymbol(new_x, new_y) != 'b' && field.getSymbol(new_x, new_y) != 'Y' && field.getSymbol(new_x, new_y) != 'E' && field.getSymbol(new_x, new_y) != 'T')
+        while (count <= 2 && field->getSymbol(new_x, new_y) != '#' && field->getSymbol(new_x, new_y) != 's' && field->getSymbol(new_x, new_y) != 'b' && field->getSymbol(new_x, new_y) != 'Y' && field->getSymbol(new_x, new_y) != 'E' && field->getSymbol(new_x, new_y) != 'T')
         {
             if (count == 1 || count == 2)
             {
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '|');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '|');
                 dash_visual.push_back({player.getX_pos(), player.getY_pos()});
                 player.setX_pos(new_x);
                 player.setY_pos(new_y);
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '@');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '@');
             }
             new_y++;
             count += 1;
@@ -83,15 +89,15 @@ void ADash::use(Player& player)
     {
         new_x++;
         count += 1;
-        while (count <= 2 && field.getSymbol(new_x, new_y) != '#' && field.getSymbol(new_x, new_y) != 's' && field.getSymbol(new_x, new_y) != 'b' && field.getSymbol(new_x, new_y) != 'Y' && field.getSymbol(new_x, new_y) != 'E' && field.getSymbol(new_x, new_y) != 'T')
+        while (count <= 2 && field->getSymbol(new_x, new_y) != '#' && field->getSymbol(new_x, new_y) != 's' && field->getSymbol(new_x, new_y) != 'b' && field->getSymbol(new_x, new_y) != 'Y' && field->getSymbol(new_x, new_y) != 'E' && field->getSymbol(new_x, new_y) != 'T')
         {
             if (count == 1 || count == 2)
             {
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '-');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '-');
                 dash_visual.push_back({player.getX_pos(), player.getY_pos()});
                 player.setX_pos(new_x);
                 player.setY_pos(new_y);
-                field.setSymbol(player.getX_pos(), player.getY_pos(), '@');
+                field->setSymbol(player.getX_pos(), player.getY_pos(), '@');
             }
             new_x++;
             count += 1;
