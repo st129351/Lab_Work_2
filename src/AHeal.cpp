7edadbb1c4ea7baf5d4ff732fb0502d1a8c1695a
Lichkovaha <st129351@student.spbu.ru>
2025-05-06 13:59:10 +0000

    improved heal amulet

--- src/AHeal.cpp
+++ src/AHeal.cpp
@@ -1,3 +1,6 @@
+//Lichkovaha Daniil
+//st129351@student.spbu.ru
+//LabWork2
 #include "AHeal.h"
 #include "Player.h"
 
@@ -12,17 +15,9 @@ AHeal::~AHeal() {}
 void AHeal::use(Player& p)
 {
     unsigned int new_health1 = p.getHealth() + heal_value;
-    unsigned int new_health2 = 100;
     if (p.getHealth() < 100)
     {
-        if (p.getHealth() > 95)
-        {
-            p.setHealth(new_health2);
-        }
-        else
-        {
-            p.setHealth(new_health1);
-        }
+        p.setHealth(new_health1);
     }
     else
     {
